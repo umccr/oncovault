0d71f8aa3e254d67372cc9e0ac7d77d0a0c09ee9
Victor San Kho Lin <victor@sankholin.com>
2026-06-04 16:29:29 +1000

    Implemented OncoVault BAMTOOLS satellite models
    * This PR implemented the metrics satellites for the BAMTOOLS tool.
      The schema is provided via OrcaGlue for Tidywigits.
    * Improved generate_hash_diff macros with hash of hashes approach
      for more resilient handling of column value in bounded context.

---
+++ models/dcl/raw_vault/bamtools/sat_bamtools_genecvggenes.sql
@@ -0,0 +1,107 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='merge',
+        unique_key=['library_workflow_run_hk', 'load_date', 'hash_diff'],
+        merge_update_columns=['last_seen_datetime'],
+        on_schema_change='fail',
+        table_type='iceberg',
+        format='parquet',
+        write_compression='zstd',
+        partitioned_by=['load_date']
+    )
+}}
+
+{% set source_table = 'bamtools_genecvggenes' %}
+
+with source as (
+
+    select
+        trim(regexp_replace(input_id, '[\n\r]+', '')) as portal_run_id,
+        trim(regexp_replace(input_pfix, '[\n\r]+', '')) as library_id,
+        trim(regexp_replace(output_id, '[\n\r]+', '')) as batch_id,
+        batch_date,
+
+        gene,
+        chrom,
+        pos_start,
+        pos_end,
+        missed_var_likelihood
+
+    from
+        {{ source('tidywigits', source_table) }}
+
+    {% if is_incremental() %}
+
+    where
+        regexp_like("$path", '{{ source_table }}\.parquet$')
+        and batch_date >= date_format(( select max(load_date) from {{ this }} ), '%Y-%m-%d')
+
+    {% else %}
+
+    where
+        regexp_like("$path", '{{ source_table }}\.parquet$')
+        and batch_date >= '{{ var("initial_batch_date") }}'
+
+    {% endif %}
+
+),
+
+encoded as (
+
+    select
+        lower(to_hex(sha256(cast(portal_run_id as varbinary)))) as workflow_run_hk,
+        lower(to_hex(sha256(cast(library_id as varbinary)))) as library_hk,
+        {{
+            generate_hash_diff([
+                'batch_id',
+                'batch_date',
+                'gene',
+                'chrom',
+                'pos_start',
+                'pos_end',
+                'missed_var_likelihood'
+            ])
+        }} as hash_diff,
+        *
+    from
+        source
+
+),
+
+transformed as (
+
+    select
+        lower(to_hex(sha256(cast(concat(workflow_run_hk, library_hk) as varbinary)))) as library_workflow_run_hk,
+        cast('{{ run_started_at.strftime("%Y-%m-%d") }}' as date) as load_date,
+        'tidywigits_{{ source_table }}' as record_source,
+        cast('{{ run_started_at }}' as timestamp) as last_seen_datetime,
+        *
+    from
+        encoded
+
+),
+
+final as (
+
+    select
+        cast(library_workflow_run_hk as varchar(64)) as library_workflow_run_hk,
+        cast(load_date as date) as load_date,
+        cast(hash_diff as varchar(64)) as hash_diff,
+        cast(record_source as varchar(255)) as record_source,
+        cast(last_seen_datetime as timestamp) as last_seen_datetime,
+        cast(batch_id as varchar(26)) as batch_id,
+        cast(batch_date as date) as batch_date,
+
+        cast(gene as varchar) as gene,
+        cast(chrom as varchar) as chrom,
+        cast(pos_start as double) as pos_start,
+        cast(pos_end as double) as pos_end,
+        cast(missed_var_likelihood as double) as missed_var_likelihood
+
+    from
+        transformed
+
+)
+
+select * from final
---
+++ models/dcl/raw_vault/bamtools/sat_bamtools_partitionstats.sql
@@ -0,0 +1,119 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='merge',
+        unique_key=['library_workflow_run_hk', 'load_date', 'hash_diff'],
+        merge_update_columns=['last_seen_datetime'],
+        on_schema_change='fail',
+        table_type='iceberg',
+        format='parquet',
+        write_compression='zstd',
+        partitioned_by=['load_date']
+    )
+}}
+
+{% set source_table = 'bamtools_partitionstats' %}
+
+with source as (
+
+    select
+        trim(regexp_replace(input_id, '[\n\r]+', '')) as portal_run_id,
+        trim(regexp_replace(input_pfix, '[\n\r]+', '')) as library_id,
+        trim(regexp_replace(output_id, '[\n\r]+', '')) as batch_id,
+        batch_date,
+
+        chrom,
+        pos_start,
+        pos_end,
+        total_reads,
+        dup_reads,
+        chim_reads,
+        interpartition,
+        unmap_reads,
+        process_time
+
+    from
+        {{ source('tidywigits', source_table) }}
+
+    {% if is_incremental() %}
+
+    where
+        regexp_like("$path", '{{ source_table }}\.parquet$')
+        and batch_date >= date_format(( select max(load_date) from {{ this }} ), '%Y-%m-%d')
+
+    {% else %}
+
+    where
+        regexp_like("$path", '{{ source_table }}\.parquet$')
+        and batch_date >= '{{ var("initial_batch_date") }}'
+
+    {% endif %}
+
+),
+
+encoded as (
+
+    select
+        lower(to_hex(sha256(cast(portal_run_id as varbinary)))) as workflow_run_hk,
+        lower(to_hex(sha256(cast(library_id as varbinary)))) as library_hk,
+        {{
+            generate_hash_diff([
+                'batch_id',
+                'batch_date',
+                'chrom',
+                'pos_start',
+                'pos_end',
+                'total_reads',
+                'dup_reads',
+                'chim_reads',
+                'interpartition',
+                'unmap_reads',
+                'process_time'
+            ])
+        }} as hash_diff,
+        *
+    from
+        source
+
+),
+
+transformed as (
+
+    select
+        lower(to_hex(sha256(cast(concat(workflow_run_hk, library_hk) as varbinary)))) as library_workflow_run_hk,
+        cast('{{ run_started_at.strftime("%Y-%m-%d") }}' as date) as load_date,
+        'tidywigits_{{ source_table }}' as record_source,
+        cast('{{ run_started_at }}' as timestamp) as last_seen_datetime,
+        *
+    from
+        encoded
+
+),
+
+final as (
+
+    select
+        cast(library_workflow_run_hk as varchar(64)) as library_workflow_run_hk,
+        cast(load_date as date) as load_date,
+        cast(hash_diff as varchar(64)) as hash_diff,
+        cast(record_source as varchar(255)) as record_source,
+        cast(last_seen_datetime as timestamp) as last_seen_datetime,
+        cast(batch_id as varchar(26)) as batch_id,
+        cast(batch_date as date) as batch_date,
+
+        cast(chrom as varchar) as chrom,
+        cast(pos_start as double) as pos_start,
+        cast(pos_end as double) as pos_end,
+        cast(total_reads as double) as total_reads,
+        cast(dup_reads as double) as dup_reads,
+        cast(chim_reads as double) as chim_reads,
+        cast(interpartition as double) as interpartition,
+        cast(unmap_reads as double) as unmap_reads,
+        cast(process_time as double) as process_time
+
+    from
+        transformed
+
+)
+
+select * from final
